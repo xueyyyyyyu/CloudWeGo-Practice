81680e0f3ad3ed1a496a0f66fba8434eef722f68
Xue Yu <211250052@smail.nju.edu.cn>
2023-07-28 00:47:08 +0800

    add map mutex

--- rpcsvr/handler.go
+++ rpcsvr/handler.go
@@ -4,16 +4,22 @@ import (
 	"context"
 	"errors"
 	demo "github.com/xueyyyyyyu/rpcsvr/kitex_gen/demo"
+	"sync"
 )
 
 // StudentServiceImpl implements the last service interface defined in the IDL.
-type StudentServiceImpl struct{}
+type StudentServiceImpl struct {
+	mu sync.Mutex // Define a mutex to protect id2Student
+}
 
 // map 缓存
 var id2Student = map[int]demo.Student{}
 
 // Register implements the StudentServiceImpl interface.
 func (s *StudentServiceImpl) Register(ctx context.Context, student *demo.Student) (resp *demo.RegisterResp, err error) {
+	s.mu.Lock()         // Lock the mutex before modifying the map
+	defer s.mu.Unlock() // Unlock the mutex after the function exits
+
 	// rpc register
 	id := int(student.Id)
 	_, found := id2Student[id]
@@ -28,6 +34,9 @@ func (s *StudentServiceImpl) Register(ctx context.Context, student *demo.Student
 
 // Query implements the StudentServiceImpl interface.
 func (s *StudentServiceImpl) Query(ctx context.Context, req *demo.QueryReq) (resp *demo.Student, err error) {
+	s.mu.Lock()         // Lock the mutex before modifying the map
+	defer s.mu.Unlock() // Unlock the mutex after the function exits
+
 	// rpc query
 	/*resp = &demo.Student{
 		Id:   1,
